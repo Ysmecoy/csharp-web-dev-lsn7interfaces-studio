ed1b8b8cb782d6b1d487f33b52af05c4145e49a5
Ysmerys <ysmecoy2@gmail.com>
2021-01-06 15:26:29 -0600

    class studio 7 with interface

---
+++ SpinningDiscs/IOpticalDisc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    interface IOpticalDisc
+    {
+        public string Spin();
+        public void StoreData();
+        public string ReportStatus();
+        public void ReadData();
+        public double GetRemainingCapacity();
+    }
+}
